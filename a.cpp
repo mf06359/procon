ceeb555feef77fa8953c56d03ac13e3b33836705
mf06359 <mightyworksmf@gmail.com>
2026-06-16 08:43:57 +0900

    modified a.cpp

--- a.cpp
+++ a.cpp
@@ -1,6 +1,7 @@
-#include <atcoder/all>
-import std;
+#include <bits/stdc++.h>
 using namespace std;
 int main() {
-    cout << "Hello, World\n";
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << "Hello World\n";
 }

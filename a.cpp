2e151c90b5c1c6d0856be17653021ce5bc636e1a
mf06359 <mightyworksmf@gmail.com>
2026-07-03 17:01:01 +0900

    flake.nix updated

--- a.cpp
+++ a.cpp
@@ -1,14 +1,33 @@
 import std;
 using namespace std;
+const long long inf = 2e18;
+void solve();
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n, tt;
-    cin >> n;
-    lazy_segment_tree<S, op, e, F, mapping, composition, id> seg(n);
-    for (;tt--;) {
-        int l, r;
-        cin >> l >> r;
-        l--;
-    }
+    int tt = 1;
+    cin >> tt;
+    while (tt--) solve();
 }
+void solve(){
+    int h, w;
+    vector<string> s(h);
+    for (int i = 0; i < h; i++) cin >> s[i];
+    vector dp(h + 1, vector<int> (w + 1, 0));
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            dp[i + 1][j + 1] += s[i] == '#';
+        }
+    }
+    for (int i = 0; i < h + 1; i++) {
+        for (int j = 0; j < w; j++) {
+            dp[i][j + 1] += dp[i][j];
+        }
+    }
+    for (int j = 0; j < w + 1; j++) {
+        for (int i = 0; i < h; i++) {
+            dp[i + 1][j] += dp[i][j];
+        }
+    }
+    
+};
